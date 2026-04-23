3adff1a19d59a8b3b028de1001aae285426166f5
aqnyutamiiidanu-a11y <aqnyutamiiidanu@gmail.com>
2026-04-23 12:54:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import joblib
 import pandas as pd
+import streamlit as st
+import joblib
+import pandas as pd
+import numpy as np
+import sklearn  # Tambahkan ini untuk memastikan library terdeteksi
+
+# Load model
+model = joblib.load("model.pkl")
 
 # Load model
 model = joblib.load("model.pkl")
